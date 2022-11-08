4115ab7e0ba4a7d818e74b55616127fb0f447090
aifuxi <aifuxi.js@gmail.com>
2022-11-08 22:49:15 +0800

    chore: next.config.js修改并添加注释

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // 关闭reactStrictMode
+  reactStrictMode: false,
   swcMinify: true,
   publicRuntimeConfig: {
+    // 配置运行时的变量 process.env
     BASE_URL: process.env.BASE_URL,
   },
+  // 配置代理，在react组件(不是在服务端渲染的时候，是在客户端的时候，如useEffect里面发送的请求)里发的请求都转发到
+  // process.env.BASE_URL里面，next会自动根据运行环境来读取.env文件获取 process.env.BASE_URL
   async rewrites() {
     return [
       {
